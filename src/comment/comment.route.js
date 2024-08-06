2be8adbb14b1f2fa510f07564e384c7a2477dece
jungmin <dbswjdals2580@gmail.com>
2024-08-07 03:33:21 +0900

    fix : get -> patch

--- src/comment/comment.route.js
+++ src/comment/comment.route.js
@@ -6,7 +6,7 @@ const commentController = new CommentController();
 
 router.post('/', commentController.createComment);
 router.get('/:board_id', commentController.getComments);
-router.put('/:comment_id', commentController.updateComment);
+router.patch('/:comment_id', commentController.updateComment);
 router.delete('/:comment_id', commentController.deleteComment);
 router.post('/:comment_id/like', commentController.toggleLike); 
 router.post('/:comment_id/report', commentController.reportComment);
