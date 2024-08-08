59fc3c92cddf481e2ccd5fd814849e510c2997e8
jungmin <dbswjdals2580@gmail.com>
2024-08-09 02:24:20 +0900

    fix : db이름 수정

--- config/response.js
+++ config/response.js
@@ -6,14 +6,3 @@ export const response = ({ isSuccess, code, message }, result = null) => {
         result: result
     };
 };
-
-// export const response = (status, result = null) => {
-//     // status는 상태 코드와 관련된 정보를 담고 있는 객체여야 함
-//     // 하지만 상태 코드는 숫자 형태여야 함
-//     return {
-//         isSuccess: status.isSuccess,
-//         code: status.code,
-//         message: status.message,
-//         result: result
-//     };
-// };
