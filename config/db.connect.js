600a6b1eb4a6f8a7e02ec95a195ecc74669566dd
chadoli27 <jasminekim1009@gmail.com>
2024-08-11 17:32:04 +0900

    undefined 오류 해결

--- config/db.connect.js
+++ config/db.connect.js
@@ -5,15 +5,14 @@ dotenv.config();
 
 
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'picopitchdb.cv6qca0emtk1.ap-northeast-2.rds.amazonaws.com',
-    user: process.env.DB_USER || 'root',
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_TABLE || 'picodb',
-    password: process.env.DB_PASSWORD || 'picopitch1234',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_TABLE,
+    password: process.env.DB_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
     connectTimeout: 60000
 });
 
-console.log('Database pool created : ', pool);
--- src/keywords/keywords.dao.js
+++ src/keywords/keywords.dao.js
@@ -1,7 +1,8 @@
-import pool from '../../config/db.connect.js';
+import { pool } from '../../config/db.connect.js';
+
 
 export const deleteKeyword = async (user_id, ppt_id, script_id) => {
-    const db = await pool.dbConnection();
+    const db = await pool.getConnection();
     const [result] = await db.execute(
         'DELETE FROM keywords WHERE user_id = ? AND ppt_id = ? AND script_id = ?',
         [user_id, ppt_id, script_id]
@@ -10,12 +11,22 @@ export const deleteKeyword = async (user_id, ppt_id, script_id) => {
 };
 
 export const insertNewKeyword = async (user_id, ppt_id, script_id, keyword) => {
-    const db = await dbConnection();
-    const [result] = await db.execute(
-        'INSERT INTO keywords (user_id, ppt_id, script_id, keyword) VALUES (?, ?, ?, ?)',
-        [user_id, ppt_id, script_id, keyword]
-    );
-    return result;
+    const db = await pool.getConnection();
+
+    try {
+        const [result] = await db.query(
+            'INSERT INTO Keywords (user_id, ppt_id, script_id, keyword) VALUES (?, ?, ?, ?)',
+            [user_id, ppt_id, script_id, keyword]
+        );
+        return result;
+    } catch (error) {
+        console.error('Error inserting keyword:', error);
+        throw error; // 에러를 다시 던져 호출한 곳에서 처리할 수 있도록 합니다.
+    } finally {
+        db.release();
+    }
+    eo
+
 };
 
 export const updateKeywordData = async (user_id, ppt_id, script_id, keyword) => {
